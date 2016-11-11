6a75d630a2f0fcdadf3c514791ffccb01ccf9a1a
eidellev <levp22@gmail.com>
2016-11-11 20:30:30 +0200

    Added type validation option and implementation

--- magiconfig.js
+++ magiconfig.js
@@ -11,24 +11,25 @@ const helper = require('./helpers/magiconfig.helper')
 /**
  * Builds a configuration object based on NODE_ENV and oprional static config
  *
- * @param   {Object}            [params]                builder params
- * @param   {String}            [params.template]       Path to config template file
- * @param   {Object}            [params.envConfig]      Key-value pairs: { 'env name': 'env config file' }, i.e. { dev: './config/dev.toml' }
- * @param   {String}            [params.staticConfig]   Path to static config
- * @param   {Function}          [params.validate]       Custom validation function
- * @param   {Array[String]}     [params.mandatoryKeys]  A list of keys that require values.
- *                                                      Add single keys, or use `*` to signify that all keys are mandatory
- *                                                      and prefix with `-` the keys you'd like to exclude
+ * @param   {Object}            [params]                    builder params
+ * @param   {String}            [params.template]           Path to config template file
+ * @param   {Object}            [params.envConfig]          Key-value pairs: { 'env name': 'env config file' }, i.e. { dev: './config/dev.toml' }
+ * @param   {String}            [params.staticConfig]       Path to static config
+ * @param   {Function}          [params.validate]           Custom validation function
+ * @param   {Boolean}           [params.doTypeValidation]   Should validate config object property types (works in conjunction with template)
+ * @param   {Array[String]}     [params.mandatoryKeys]      A list of keys that require values.
+ *                                                          Add single keys, or use `*` to signify that all keys are mandatory
+ *                                                          and prefix with `-` the keys you'd like to exclude
  *
- * @return  {Object}                                    A merged config object (static config + env config)
+ * @return  {Object}                                        A merged config object (static config + env config)
  */
-function build (params) {
+function magiconfig (params) {
     const env          = helper.getEnv()
     const envConfig    = helper.getEnvConfig(params.envConfig, env)
     const template     = helper.getTemplate(params.templateFile)
     const staticConfig = helper.getStaticConfig(params.staticConfig)
 
-    helper.validateAgainstTemplate(envConfig, template)
+    helper.validateAgainstTemplate(envConfig, template, params.doTypeValidation || false)
     helper.validateMandatoryKeys(envConfig, params.mandatoryKeys)
 
     const finalConfig = _.merge(staticConfig, envConfig)
@@ -37,4 +38,4 @@ function build (params) {
     return finalConfig
 }
 
-module.exports = build
+module.exports = magiconfig
